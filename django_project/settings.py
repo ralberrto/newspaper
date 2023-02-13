0583c4ea67aea78899940bf19aaeec683f6bf0d4
Rafael Alberto Larios <albertolarios@me.com>
2023-02-13 14:09:11 -0600

    Add CustomUser model to project
    It's important not to run the makemigrations command before doing this step

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Custom user code
+AUTH_USER_MODEL = "accounts.CustomUser"
