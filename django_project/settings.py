26a11af264a6449e56e63d4202996aeeac3dc63b
Rafael Alberto Larios <albertolarios@me.com>
2023-02-14 21:47:34 -0600

    Install django-crispy-forms package to restyle signup form

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "crispy_forms",
+    # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
 ]
