b47b47a282515385ea98be65f73b0844e4fbc301
Rafael Alberto Larios <albertolarios@me.com>
2023-02-16 16:46:11 -0600

    Add template, view and url to render article details page

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Article
 
 
 class ArticleListView(ListView):
     model = Article
     template_name = "article_list.html"
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
