68ef24b36e5fc7a98a34afc5e7efea1fb5965e93
Rafael Alberto Larios <albertolarios@me.com>
2023-02-16 22:24:50 -0600

    Add authorization test (user) to delete and update views

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Article
 
@@ -16,7 +16,7 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = (
         "title",
@@ -24,11 +24,19 @@ class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     )
     template_name = "article_edit.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
