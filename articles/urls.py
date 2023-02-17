c4de30acfdd03929dd2fdc0d828b7c973598bef8
Rafael Alberto Larios <albertolarios@me.com>
2023-02-16 21:35:55 -0600

    Add template, view and url to render crete article form

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,7 @@ from .views import (
     ArticleDetailView,
     ArticleUpdateView,
     ArticleDeleteView,
+    ArticleCreateView,
 )
 
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
     path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
+    path("new/", ArticleCreateView.as_view(), name="article_new"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Article
@@ -28,3 +28,13 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = (
+        "author",
+        "title",
+        "body",
+    )
