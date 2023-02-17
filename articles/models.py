aad8ae3cbb5ff49cae08f4601b2fedf7bc9b8f8c
Rafael Alberto Larios <albertolarios@me.com>
2023-02-17 07:55:52 -0600

    Create Comment model, add it to admin and migrate

--- articles/models.py
+++ articles/models.py
@@ -17,3 +17,21 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+    )
+    body = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.body
+
+    def get_absolute_url(self):
+        return reverse("article_list")
